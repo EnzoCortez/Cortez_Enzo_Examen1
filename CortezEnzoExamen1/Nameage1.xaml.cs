5afe04813b309e7d20094a39c8249288844b4ced
Enzo Cortez <enzo.cortez@udla.edu.ec>
2024-12-03 08:39:16 -0500

    Creacion de la segunda pagina

---
+++ CortezEnzoExamen1/Nameage1.xaml.cs
@@ -0,0 +1,12 @@
+using Microsoft.Maui.Controls;
+
+namespace CortezEnzoExamen1
+{
+    public partial class NamePage : ContentPage
+    {
+        public NamePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
